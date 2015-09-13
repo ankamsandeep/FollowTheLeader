5fda64ac2e6d6b1eff10945db6b0096e2cb9cd3c
Sandeep Ankam <sandeepankam@me.com>
2015-09-12 20:35:01 -0700

    Working copy.

---
+++ FollowTheLeader/ConfigurationViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  FollowTheLeader
+//
+//  Created by Sandeep Ankam on 9/12/15.
+//  Copyright (c) 2015 Sandeep Ankam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigurationViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *followersCountTextBox;
+
+@end
+
---
+++ FollowTheLeader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FollowTheLeader
+//
+//  Created by Sandeep Ankam on 9/12/15.
+//  Copyright (c) 2015 Sandeep Ankam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
